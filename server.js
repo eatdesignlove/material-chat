3377573ac97db925388830727d7b3898e7b4b805
eatdesignlove <eatdesignlove@gmail.com>
2016-09-02 16:03:17 +0900

    material-chat simple prototype is completed

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require("express");
-
 var app = express();
 
-
 var http = require("http").Server(app);
+var io   = require("socket.io")(http);
 
 
 app.set("port", process.env.PROT || 3030);
 
 app.get("/", function(req, res){
-  res.send("This is Chat server");
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+
+io.on("connection", function(socket){
+  socket.on("chat message", function(msg){
+    console.log( "message:" + msg );
+    io.emit("chat message",  msg);
+  })
 });
 
 http.listen(app.get("port"), function(){
